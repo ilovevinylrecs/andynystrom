5b1bb041b81ca7896458135c4cb6539e7fc90268
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-03-06 13:36:37 -0500

    starts projects section of site

---
+++ pages/projects/index.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Projects() {
+    return (
+        <p>test</p>
+        // < Image src="/trivia-app.png" alt="Alex-Trebek-drawing" width="300" height="300" />
+    )
+};

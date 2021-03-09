f83913b5643f567ffe6d99c128cd9e34ef3d5978
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-03-08 22:51:10 -0500

    adds projects to homepage and starts projects landing page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,5 @@
-import Image from "next/image";
-
 export default function Projects() {
     return (
-        <p>test</p>
-        // < Image src="/trivia-app.png" alt="Alex-Trebek-drawing" width="300" height="300" />
+        <h3>COMING SOON</h3>
     )
 };

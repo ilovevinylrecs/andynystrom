2aafb446eef244d89c45362b28780ed64bd4fca5
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-04-10 17:46:21 -0400

    removes navigation menu on homepage, bootstrap styling, fixes navbar on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,39 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-const Menu = () => {
-  return <div className="section align-items-start row">
-    <Link href="/about">
-      <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
-        ABOUT</a>
-    </Link>
+// const Menu = () => {
+//   return <div className="section align-items-start row">
+//     <Link href="/about">
+//       <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
+//         ABOUT</a>
+//     </Link>
 
-    <Link href="/flatfile">
-      <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
-        FLATFILE</a>
-    </Link>
+//     <Link href="/flatfile">
+//       <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
+//         FLATFILE</a>
+//     </Link>
 
-    <a href="https://github.com/ilovevinylrecs" className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
-      GITHUB</a>
+//     <a href="https://github.com/ilovevinylrecs" className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
+//       GITHUB</a>
 
-    <Link href="/messages">
-      <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
-        MESSAGES
-      </a>
-    </Link>
+//     <Link href="/messages">
+//       <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
+//         MESSAGES
+//       </a>
+//     </Link>
 
-    <Link href="/projects">
-      <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
-        PROJECTS
-      </a>
-    </Link>
+//     <Link href="/projects">
+//       <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
+//         PROJECTS
+//       </a>
+//     </Link>
 
-    <Link href="/vinyl">
-      <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
-        VINYL</a>
-    </Link>
-  </div>
-}
+//     <Link href="/vinyl">
+//       <a className="h2 col-lg align-items-center" style={{ color: "black" }} target="_blank">
+//         VINYL</a>
+//     </Link>
+//   </div>
+// }
 
 export default function Home() {
   return (
@@ -41,14 +41,14 @@ export default function Home() {
       <Head>
         <title>Andy Nystrom - Home</title>
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+        {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" /> */}
       </Head>
 
       <main>
         <h1 className="title container-fluid align-items-center">
           ANDY NYSTROM
         </h1>
-        <Menu />
+        {/* <Menu /> */}
       </main>
 
       <style jsx>{`

e36c6af2ad46650fe0d3d70af239079a07038d40
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-04-16 22:30:06 -0400

    renames userAgent and versionDisconnec and moves them outside function, renames fetchCollectionAPI, adds promise void to fetchCollectionAPI, uses template string to form userAgentVersionDisconnect, removes await from collectionDataBase

--- collection-export/collection-export.ts
+++ collection-export/collection-export.ts
@@ -3,27 +3,28 @@ const Discogs = require('disconnect').Client;({
     consumerSecret: process.env.DISCOGS_CONSUMER_SECRET
 });
 
-export const fetchCollection = async () =>  {
-        const userAgent = "ilovevinylrecs";
-        const versionDisconnect = "/1.2.2";
-        const userAgentVersionDisconnect = userAgent.concat(versionDisconnect);
+const DISCOGS_USERNAME = "ilovevinylrecs";
+const DISCOGS_API_VERSION = "1.2.2";
 
-        const collectionDataBase = await new Discogs(userAgentVersionDisconnect, {userToken: process.env.DISCOGS_USER_TOKEN}).user().collection();
+export const fetchCollectionAPI = async (): Promise<void> =>  {
+    const userAgentVersionDisconnect = `${DISCOGS_USERNAME}/${DISCOGS_API_VERSION}`;
 
-        let done = false;
-        let collection = [];
-        let page = 1;
-        while (done === false) {
-            const data = await collectionDataBase.getReleases(userAgent, 0, {page: page, per_page: 500, sort: 'artist', sort_order: 'asc'}); 
-            
-            collection = collection.concat(data.releases);
-            if (page === data.pagination.pages) {
-            done = true
-            }
-            page++; 
+    const collectionDataBase = new Discogs(userAgentVersionDisconnect, {userToken: process.env.DISCOGS_USER_TOKEN}).user().collection();
+
+    let done = false;
+    let collection = [];
+    let page = 1;
+    while (done === false) {
+        const data = await collectionDataBase.getReleases(DISCOGS_USERNAME, 0, {page: page, per_page: 500, sort: 'artist', sort_order: 'asc'}); 
         
-        console.log(collection, collection.length);     
+        collection = collection.concat(data.releases);
+        if (page === data.pagination.pages) {
+        done = true
         }
+        page++; 
+    
+    console.log(collection, collection.length);     
+    }
 }
 
-fetchCollection();
+fetchCollectionAPI();

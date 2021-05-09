afaa7842289f11bcfd698d6c274fc9eaec546629
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-05-09 01:01:13 -0400

    changes postgres insert to single statement and adds to tsconfig

--- collection-export/collection-export-postgres.ts
+++ collection-export/collection-export-postgres.ts
@@ -10,7 +10,6 @@ const Discogs = require('disconnect').Client;({
 const DISCOGS_USERNAME = "ilovevinylrecs";
 const DISCOGS_API_VERSION = "1.2.2";
 
-//comment out export and promise<void> in order to get script to run
 export const fetchCollectionAPI = async (): Promise<void> =>  {
     const userAgentVersionDisconnect = `${DISCOGS_USERNAME}/${DISCOGS_API_VERSION}`;
 
@@ -31,61 +30,45 @@ export const fetchCollectionAPI = async (): Promise<void> =>  {
 
     const pool = new Pool();
 
-    //checks to see if table exists, if not then builds new_releases table - this is a placeholder, code doesn't work yet
-    // if (!pool.query(`SELECT EXISTS (SELECT FROM new_releases)`)) {
-    //         pool.connect()
-    //             .then(client => {
-    //                 return client.query(`CREATE TABLE new_releases 
-    //                 (id VARCHAR (1000) PRIMARY KEY,
-    //                 instance_id VARCHAR (1000) NULL,
-    //                 date_added VARCHAR (1000) NULL,
-    //                 rating VARCHAR (1000) NULL,
-    //                 basic_information_id VARCHAR (1000) NULL,
-    //                 basic_information_master_id VARCHAR (1000) NULL,
-    //                 basic_information_master_url VARCHAR (1000) NULL,
-    //                 basic_information_resource_url VARCHAR (1000) NULL,
-    //                 basic_information_thumb VARCHAR (1000) NULL,
-    //                 basic_information_cover_image VARCHAR (1000) NULL,
-    //                 basic_information_title VARCHAR (1000) NULL,
-    //                 basic_information_year VARCHAR (4) NULL,
-    //                 basic_information_formats VARCHAR (1000) NULL,
-    //                 basic_information_labels VARCHAR (1000) NULL,
-    //                 basic_information_artists VARCHAR (1000) NULL,
-    //                 basic_information_genres VARCHAR (1000) NULL,
-    //                 basic_information_styles VARCHAR (1000) NULL
-    //                 ;`)
-    //             })
-    // }
-
     //deletes old data in new_releases table
     await pool.connect()
         .then(client => {
             return client.query("DELETE FROM new_releases")
         })
 
-    //loops through discogs API and inserts data into new_releases table
+    let output = "";
+
+    //loops through discogs API and stores values in output string
     for (let i = 0; i < collection.length; i++) {
         let title = collection[i].basic_information.title.replace(/'/g, "''");
         let format = collection[i].basic_information.formats[0].name.replace(/'/g, "''");
         let label = collection[i].basic_information.labels[0].name.replace(/'/g, "''");
         let artist = collection[i].basic_information.artists[0].name.replace(/'/g, "''");
         let genre = collection[i].basic_information.genres[0].replace(/'/g, "''");
-  
-        let query = `INSERT INTO new_releases (id, instance_id, date_added, rating, basic_information_id, 
+
+        let values = `('${collection[i].id}', '${collection[i].instance_id}', '${collection[i].date_added}',
+        '${collection[i].rating}', '${collection[i].basic_information.id}', '${collection[i].basic_information.master_id}',
+        '${collection[i].basic_information.master_url}', '${collection[i].basic_information.resource_url}',
+        '${collection[i].basic_information.thumb}', '${collection[i].basic_information.cover_image}', '${title}',
+        '${collection[i].basic_information.year}', '${format}', '${label}',
+        '${artist}', '${genre}', '${collection[i].basic_information.styles}'),`
+
+        output += values;
+    }
+
+    //creates new string removing trailing comma in output string
+    let newOutput = output.slice(0, -1);
+
+    //postgres insert statement    
+    let query = `INSERT INTO new_releases (id, instance_id, date_added, rating, basic_information_id, 
             basic_information_master_id, basic_information_master_url, basic_information_resource_url, 
             basic_information_thumb, basic_information_cover_image, basic_information_title, basic_information_year, 
             basic_information_formats, basic_information_labels, basic_information_artists, basic_information_genres, basic_information_styles) 
-            VALUES ('${collection[i].id}', '${collection[i].instance_id}', '${collection[i].date_added}',
-            '${collection[i].rating}', '${collection[i].basic_information.id}', '${collection[i].basic_information.master_id}',
-            '${collection[i].basic_information.master_url}', '${collection[i].basic_information.resource_url}',
-            '${collection[i].basic_information.thumb}', '${collection[i].basic_information.cover_image}', '${title}',
-            '${collection[i].basic_information.year}', '${format}', '${label}',
-            '${artist}', '${genre}', '${collection[i].basic_information.styles}');`
-
-        await pool.query(query, (err, res) => {
-            console.log(err, res)
-        })
-    }
+            VALUES ${newOutput};`
+ 
+    await pool.query(query, (err, res) => {
+        console.log(err, res)
+    })
 }
 
 fetchCollectionAPI();
